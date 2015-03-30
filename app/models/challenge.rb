5f1b96eee2b2623223bf7c0894642c8d8f328ee9
Iain Watt <iainjameswatt@gmail.com>
2015-03-30 14:50:53 +0100

    added assocation with challenge to both court and centre

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,7 +2,9 @@ class Challenge < ActiveRecord::Base
 
   belongs_to :user, class_name: 'User', foreign_key: 'player1'
   belongs_to :user, class_name: 'User', foreign_key: 'player2'
-  belongs_to :court (or centre through courts)
+  belongs_to :centre
+  belongs_to :court
+ 
 
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326195107) do
+ActiveRecord::Schema.define(version: 20150330134857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150326195107) do
     t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "centre_id"
+    t.integer  "court_id"
   end
 
   create_table "courts", force: :cascade do |t|
