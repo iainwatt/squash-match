902d1aa74ed23ebdd8c8d59eb3a711973bb87218
Iain Watt <iainjameswatt@gmail.com>
2015-04-21 12:11:32 +0100

    migrated age to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   before_create :set_default_elo
+   # before_create :set_default_elo
    before_create :set_default_tagline
    before_create :set_ninja_rank
    before_save :set_default_tagline
@@ -28,13 +28,11 @@ class User < ActiveRecord::Base
   has_many(:reverse_challenges, :class_name => :Challenge, :foreign_key => :player2_id, :dependent => :destroy)
 
 
-  def set_default_elo
-    self.elo = 1000
-  end
+  # def set_default_elo
+  #   self.elo = 1000
+  # end
 
-  def set_default_image 
-    self.user_image = 'blanca/jpg'
-  end
+
 
   def set_default_tagline
     self.tagline = ["I came here to do 2 things, drink beer and kick ass...", "You talking to me ?", "You’re a funny guy Sully, I like you. That’s why I’m going to kill you last", "Go ahead make my day", "Do I feel lucky…well, do ya punk ?", "I eat Green Berets for breakfast...", "Come with me if you want to live", "Where's my money? I'm getting real tired of you duckin' me..", "Put the bunny back in the box..", "Sh*t just got real..", "you cant be serious", "I thought it was out....", "The most decorated captain in the black berets", "Run fast or be last", "Some call them opponents, I call them victims.", "I eat lightning and crap thunder", "Losers complain…champions train", "My warm up is your work out."].sample
